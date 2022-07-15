59a9e9ce053b6536864b1ebbb91f12dcf935da07
Tayanne <tayh.snovaes@gmail.com>
2022-07-15 20:28:30 -0300

    add theme

---
+++ src/styles/theme.js
@@ -0,0 +1,20 @@
+export const theme = {
+   white: '#FFFFFF',
+   red: '#FF6262',
+
+   'black-medium': '#1A1A1A',
+   'black-dark': '#000000',
+
+   'gray-light': '#DDDDDD',
+   'gray-dark': '#5E5E5E',
+
+   'blue-light': '#71C3FF',
+   'blue-medium': '#33A4F5',
+   'blue-dark': '#004170',
+
+   'green-light': '#729F92',
+   'green-medium': '#76A866',
+
+   'brown-light': '#EAAB7D',
+   'brown-medium': '#BF9762',
+};
