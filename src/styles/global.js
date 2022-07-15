4d388674b708567c80853e853b46fa4143debbfe
Tayanne <tayh.snovaes@gmail.com>
2022-07-15 20:28:38 -0300

    add global style

---
+++ src/styles/global.js
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components';
+
+import { theme } from './theme';
+
+export const GlobalStyle = createGlobalStyle`
+   * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+   }
+
+   body {
+      background: ${theme['gray-dark']};
+      color: ${theme.white};
+   }
+
+   body, input, button {
+      font-family: 'Poppins', sans-serif;
+      font-weight: 700;
+      font-size: 1rem;
+   }
+`;
