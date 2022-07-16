c06d3292110e4af849a00a1d9b9e9c4d47a0543b
Tayanne <tayh.snovaes@gmail.com>
2022-07-16 16:38:56 -0300

    [fix]Header: Button style

--- src/components/header/Header.style.js
+++ src/components/header/Header.style.js
@@ -29,11 +29,14 @@ export const Logo = styled.img`
 `;
 
 export const Button = styled.button`
-   width: 287px;
+   width: 200px;
    height: 74px;
    border-radius: 8px;
    border: 0;
    color: ${theme.white};
+
+   font-size: ${(props) => (props.type === 'Pokédex' ? '24px' : '16px')};
+   font-weight: ${(props) => (props.type === 'Pokédex' ? '700' : '400')};
 
    background: ${(props) =>
       props.type === 'Pokédex' ? theme['blue-medium'] : theme.red};
