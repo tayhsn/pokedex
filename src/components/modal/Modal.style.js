cf4cfc528e1fdaf93b4ce15cdafecae6effe8e1d
Tayanne <tayh.snovaes@gmail.com>
2022-07-16 13:09:16 -0300

    done Modal component: layout and logic

---
+++ src/components/modal/Modal.style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { theme } from '../../styles/theme';
+
+export const ModalContainer = styled.div`
+   width: 451px;
+   height: 222px;
+   border-radius: 12px;
+
+   background: ${theme.white};
+   color: ${theme['black-dark']};
+`;
