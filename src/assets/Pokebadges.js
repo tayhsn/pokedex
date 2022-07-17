127100a47ebeaeec5650e502d9977d8232fa8eb4
Tayanne <tayh.snovaes@gmail.com>
2022-07-17 15:02:38 -0300

    [fix] move Pokebadge to assets

---
+++ src/assets/Pokebadges.js
@@ -0,0 +1,17 @@
+import bug from './images/bug.png';
+import fire from './images/fire.png';
+import flying from './images/flying.png';
+import grass from './images/grass.png';
+import normal from './images/normal.png';
+import poison from './images/poison.png';
+import water from './images/water.png';
+
+export const POKEMON_BADGES = {
+   BUG: bug,
+   FIRE: fire,
+   FLYING: flying,
+   GRASS: grass,
+   NORMAL: normal,
+   POISON: poison,
+   WATER: water,
+};
