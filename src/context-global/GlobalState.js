c36e4504aa06dc745877f557d6312647050edfee
Tayanne <tayh.snovaes@gmail.com>
2022-07-18 13:12:04 -0300

    [feat] global state

--- src/context-global/GlobalState.js
+++ src/context-global/GlobalState.js
@@ -1,32 +1,33 @@
-import axios from "axios";
-import React, { useState } from "react";
-import { GlobalStateContext } from "./GlobalStateContext";
+import axios from 'axios';
+import React, { useState } from 'react';
+import { GlobalStateContext } from './GlobalStateContext';
 
-const URL = 'https://pokeapi.co/api/v2/pokemon'
+const URL = 'https://pokeapi.co/api/v2/pokemon';
 
 export const GlobalState = (props) => {
-    
-    const [pokemons, setPokemons] = useState([])
-    const [pokemonDetails, setPokemonDetails] = useState([])
+   const [pokemons, setPokemons] = useState([]);
+   const [pokemonDetails, setPokemonDetails] = useState([]);
 
-    const getAllPokemons = () => {
-        axios.get(URL) 
-        .then((response) => setPokemons(response.data.results))
-        .catch((error) => error.message)
-    }
+   const getAllPokemons = () => {
+      axios
+         .get(URL)
+         .then((response) => setPokemons(response.data.results))
+         .catch((error) => error.message);
+   };
 
-    const getPokemonById= (id) => {
-        axios.get(`${URL}/${id}`)
-        .then((response) => setPokemonDetails(response.data))
-        .catch((error) => console.log(error.message))
-    }
-    
-    const states = { pokemons, pokemonDetails }
-    const requests = { getAllPokemons, getPokemonById }
+   const getPokemonById = (id) => {
+      axios
+         .get(`${URL}/${id}`)
+         .then((response) => setPokemonDetails(response.data))
+         .catch((error) => console.log(error.message));
+   };
 
-    return(
-        <GlobalStateContext.Provider value={{ states, requests }}>
-            {props.children}
-        </GlobalStateContext.Provider>
-    )
-}
+   const states = { pokemons, pokemonDetails };
+   const requests = { getAllPokemons, getPokemonById };
+
+   return (
+      <GlobalStateContext.Provider value={{ states, requests }}>
+         {props.children}
+      </GlobalStateContext.Provider>
+   );
+};
