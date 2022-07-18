14d10991873958d04e7444abc3d671889bc48dec
PatiSacramento <patricia.sacramento@labenu.com.br>
2022-07-18 13:35:59 -0300

    [add] new request

--- src/context-global/GlobalState.js
+++ src/context-global/GlobalState.js
@@ -7,23 +7,33 @@ const URL = 'https://pokeapi.co/api/v2/pokemon';
 export const GlobalState = (props) => {
    const [pokemons, setPokemons] = useState([]);
    const [pokemonDetails, setPokemonDetails] = useState([]);
+   const [pokemonImages, setPokemonImages] = useState([])
 
    const getAllPokemons = () => {
       axios
          .get(URL)
-         .then((response) => setPokemons(response.data.results))
+         .then((response) => {
+            setPokemons(response.data.results)
+
+            let pokeArray = []
+            response.data.results.map(async(poke) =>{
+                const response = await axios.get(poke.url)
+                pokeArray = [...pokeArray, { name: poke.name, image: response.data.sprites.front_default, types: response.data.types}]
+                console.log(pokeArray)
+            })
+        })
          .catch((error) => error.message);
    };
 
-   const getPokemonById = (id) => {
+   const getPokemonByName = (name) => {
       axios
-         .get(`${URL}/${id}`)
+         .get(`${URL}/${name}`)
          .then((response) => setPokemonDetails(response.data))
          .catch((error) => console.log(error.message));
    };
 
-   const states = { pokemons, pokemonDetails };
-   const requests = { getAllPokemons, getPokemonById };
+   const states = { pokemons, pokemonDetails, pokemonImages };
+   const requests = { getAllPokemons, getPokemonByName };
 
    return (
       <GlobalStateContext.Provider value={{ states, requests }}>
