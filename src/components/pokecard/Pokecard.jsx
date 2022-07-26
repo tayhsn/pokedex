9a41d30935bfd93e0576101748a4e619ed354c79
Tayanne <tayh.snovaes@gmail.com>
2022-07-26 14:13:07 -0300

    [feat]

--- src/components/pokecard/Pokecard.jsx
+++ src/components/pokecard/Pokecard.jsx
@@ -1,39 +1,38 @@
+import { useNavigate } from "react-router-dom"
+import { goToDetailsPage } from "../../router/coordinator"
 import {
-   ActionContainer,
-   Ancor,
-   Button,
-   InfoContainer,
-   ImageContainer,
-   Image,
-   PokecardContainer,
-} from './Pokecard.style';
-import { POKEMON_BADGES } from '../../assets/Pokebadges';
-import { goToDetailsPage } from '../../router/coordinator';
-import { useNavigate } from 'react-router-dom';
+  ActionContainer,
+  Ancor,
+  Button,
+  Image,
+  ImageContainer,
+  InfoContainer,
+  PokecardContainer,
+} from "./Pokecard.style"
 
 export const Pokecard = (props) => {
-   const navigate = useNavigate();
+  const navigate = useNavigate()
 
-   const badges = props.types.map((type) => console.log(type.type.name));
+  const badges = props.types.map((type) => console.log(type.type.name))
 
-   return (
-      <PokecardContainer>
-         <InfoContainer>
-            <div>
-               <p>#{props.id}</p>
-               <h1>{props.name}</h1>
-            </div>
-            <div></div>
-         </InfoContainer>
-         <ImageContainer>
-            <Image src={props.image} />
-         </ImageContainer>
-         <ActionContainer>
-            <Ancor href={() => goToDetailsPage(navigate, props.name)}>
-               Detalhes
-            </Ancor>
-            <Button action={props.action}>{props.action}!</Button>
-         </ActionContainer>
-      </PokecardContainer>
-   );
-};
+  return (
+    <PokecardContainer>
+      <InfoContainer>
+        <div>
+          <p>#{props.id}</p>
+          <h1>{props.name}</h1>
+        </div>
+        <div></div>
+      </InfoContainer>
+      <ImageContainer>
+        <Image src={props.image} />
+      </ImageContainer>
+      <ActionContainer>
+        <Ancor href={() => goToDetailsPage(navigate, props.name)}>
+          Detalhes
+        </Ancor>
+        <Button action={props.action}>{props.action}!</Button>
+      </ActionContainer>
+    </PokecardContainer>
+  )
+}
